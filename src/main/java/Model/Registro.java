7cf68a45c169fa001f7c3d7aa3fe9bd63b790b0d
JoshuaJSR <joshjared2004@gmail.com>
2025-12-20 10:39:04 -0600

    creacion de getters y setters, conexion a base de datos

--- src/main/java/Model/Registro.java
+++ src/main/java/Model/Registro.java
@@ -41,5 +41,98 @@ public class Registro {
         this.fechaRegistro = LocalDate.now();
     }
 
+    //Constructores
+    public Registro() {}
 
+    public Registro(Long idRegistro, String poliza, String codigoProducto, int numeroLote, BigDecimal unidades, String ubicacion, LocalDate fechaRegistro, Usuario usuario) {
+        this.idRegistro = idRegistro;
+        this.poliza = poliza;
+        this.codigoProducto = codigoProducto;
+        this.numeroLote = numeroLote;
+        this.unidades = unidades;
+        this.ubicacion = ubicacion;
+        this.fechaRegistro = fechaRegistro;
+        this.usuario = usuario;
+    }
+
+    //getters y setters
+
+    public Long getIdRegistro() {
+        return idRegistro;
+    }
+
+    public void setIdRegistro(Long idRegistro) {
+        this.idRegistro = idRegistro;
+    }
+
+    public String getPoliza() {
+        return poliza;
+    }
+
+    public void setPoliza(String poliza) {
+        this.poliza = poliza;
+    }
+
+    public String getCodigoProducto() {
+        return codigoProducto;
+    }
+
+    public void setCodigoProducto(String codigoProducto) {
+        this.codigoProducto = codigoProducto;
+    }
+
+    public int getNumeroLote() {
+        return numeroLote;
+    }
+
+    public void setNumeroLote(int numeroLote) {
+        this.numeroLote = numeroLote;
+    }
+
+    public BigDecimal getUnidades() {
+        return unidades;
+    }
+
+    public void setUnidades(BigDecimal unidades) {
+        this.unidades = unidades;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    //ToString
+
+    @Override
+    public String toString() {
+        return "Registro{" +
+                "idRegistro=" + idRegistro +
+                ", poliza='" + poliza + '\'' +
+                ", codigoProducto='" + codigoProducto + '\'' +
+                ", numeroLote=" + numeroLote +
+                ", unidades=" + unidades +
+                ", ubicacion='" + ubicacion + '\'' +
+                ", fechaRegistro=" + fechaRegistro +
+                '}';
+    }
 }
--- src/main/java/Model/Usuario.java
+++ src/main/java/Model/Usuario.java
@@ -31,6 +31,65 @@ public class Usuario {
         this.fechaRegistrado = LocalDate.now();
     }
 
+    //Constructores
+    public Usuario() {}
 
+    public Usuario(Long idUsuario, String nombre, String password, LocalDate fechaRegistrado, List<Registro> registros) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.password = password;
+        this.fechaRegistrado = fechaRegistrado;
+        this.registros = registros;
+    }
+    //getters y setters
 
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getFechaRegistrado() {
+        return fechaRegistrado;
+    }
+
+    public void setFechaRegistrado(LocalDate fechaRegistrado) {
+        this.fechaRegistrado = fechaRegistrado;
+    }
+
+    public List<Registro> getRegistros() {
+        return registros;
+    }
+
+    public void setRegistros(List<Registro> registros) {
+        this.registros = registros;
+    }
+    //ToString
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "idUsuario=" + idUsuario +
+                ", nombre='" + nombre + '\'' +
+                ", fechaRegistrado=" + fechaRegistrado +
+                '}';
+    }
 }
